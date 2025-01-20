571436df13e3da5556f14a1fbfb391a06f585b53
darkmatter <vikasanand.darkmatter@gmail.com>
2025-01-20 21:15:28 +0530

    Navbar structure

--- frontend/src/components/LandingPage/Navbar.js
+++ frontend/src/components/LandingPage/Navbar.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='bg-black h-20 flex gap-10 items-center justify-start p-4'>
+
+        <div className='text-white text-3xl font-bold'>Engine</div>
+
+        <div className='flex gap-10'>
+            {[{name: 'Home', link: '/home'}, {name: 'About', link: '/about'}, {name: 'Contact', link: '/contact'}].map((item, index) => (
+                <div>
+                    {item.name}
+                </div>
+            ))}
+        </div>
+
+            <div>
+                Get Started
+            </div>
+    </div>
   )
 }
 
--- frontend/src/pages/LandingPage/index.js
+++ frontend/src/pages/LandingPage/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../../components/LandingPage/Navbar.js';
 
-function DashBord() {
+function LandingPage() {
   return (
     <div className="flex flex-col w-full h-full">
-      
+      <Navbar/>
     </div>
   )
 }
 
-export default DashBord
+export default LandingPage
