ef6b64f1674782984ceb2b98af7708c73742c091
darkmatter <vikasanand.darkmatter@gmail.com>
2025-01-27 17:00:42 +0530

    feat: implement responsive navigation toggle in Sidebar and adjust layout in FirstChat component

--- frontend/src/pages/Home/firstChat.js
+++ frontend/src/pages/Home/firstChat.js
@@ -1,19 +1,20 @@
-import React from 'react'
 import Sidebar from '../../components/Home/Sidebar'
 import ChatSection from '../../components/Home/ChatSection'
 import FirstChatSection from '../../components/Home/firstChat'
+import { useState } from 'react'
 
 const FirstChat = () => {
+ 
+
   return (
-    
      <div className="flex h-screen w-full border-r-[0.2px] border-white">
      {/* Nav should take 20% of the width and remain fixed */}
-     <div className="md:w-[20%] ">
+     <div className="hidden md:flex md:w-[20%] ">
        <Sidebar />
      </div>
      
      {/* The Hero content should take up the remaining 80% and be scrollable */}
-     <div className="md:w-[80%]  flex gap-5 flex-col items-center justify-center ">
+     <div className="w-full md:w-[80%]  flex gap-5 flex-col items-center justify-center ">
         <div className=' text-white text-3xl font-bold '>
             What do you want to create today?
         </div>
