e38b43237d3c48f7a35a3880db27a54b8ccce495
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2025-01-30 07:24:09 +0530

    feat: enhance Testimonials section with gradient background and layout adjustments

--- frontend/src/components/LandingPage/section/Testimonials.js
+++ frontend/src/components/LandingPage/section/Testimonials.js
@@ -29,9 +29,10 @@ const Testimonials = () => {
             <div className='flex flex-col  lg:-mt-48 gap-2 lg:w-[full] sm:-mt-12'>
                 <Heading heading={"Testimonials"}/>             
                 <Desc tag1={"Our AI Capabilities"} tag2={"Discover the cutting-edge capabilities of our AI solutions designed to transform your business operations."}/>
-                
-                <div className='mx-auto bg-white bg-opacity-5 rounded-lg p-4 h-[720px] w-full flex gap-3'>
-                    <div className="vertical-slider md:w-1/3 flex flex-col gap-3">
+                <div className=' overflow-hidden bg-gradient-to-b from-[#474747] to-[#1d1d1d] border rounded-full  z-20 h-[500px] '>
+                <div className=' flex gap-3'>
+                    <div className="vertical-slider md:w-1/3  flex flex-col gap-3">
+                    
                         <div className="slider-content">
                             {
                             [...tweets, ...tweets].map((tweet, index) => (
@@ -51,7 +52,7 @@ const Testimonials = () => {
                         </div>
                     </div>
 
-                    <div className='vertical-silder md:w-1/3 hidden md:flex md:flex-col overflow-y-hidden md:gap-3'>
+                    <div className='vertical-silder md:w-1/3 hidden  md:flex md:flex-col overflow-y-hidden md:gap-3'>
                         <div className="reverse-slider-content">
                             {
                                 [...tweets, ...tweets].map((tweet, index) => (
@@ -72,7 +73,7 @@ const Testimonials = () => {
                     </div>
 
 
-                    <div className='vertical-sider md:w-1/3 hidden md:flex md:flex-col overflow-y-hidden md:gap-3'>
+                    <div className='vertical-sider md:w-1/3 hidden  md:flex md:flex-col overflow-y-hidden md:gap-3'>
                         <div className="slider-content">
                             {
                                 [...tweets, ...tweets].map((tweet, index) => (
@@ -91,7 +92,7 @@ const Testimonials = () => {
                             }
                         </div>
                     </div>
-                </div>
+                </div></div>
                 
             </div>
         </div>
