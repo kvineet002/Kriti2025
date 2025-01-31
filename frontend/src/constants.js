0cf9251a7f520af9e65d788503dc8e4dc5f69a79
darkmatter <vikasanand.darkmatter@gmail.com>
2025-01-31 17:09:36 +0530

    minor fix

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,13 +1,3 @@
-export const firebaseApi = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
-};
-
 //backend url paste it here
 export const BASE_URL = "http://localhost:3002";
 
