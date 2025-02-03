4914b808d5064e45a37dcb805f5a9e48bf3e0db4
Vikas Anand <117939614+darkm4tter07@users.noreply.github.com>
2025-02-03 20:20:39 +0530

    Delete backend directory

--- backend/handlers/jwtHandler.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-exports.createAccessToken = (email,name,avatar) => {
-    return jwt.sign(
-        {email: email,name: name,avatar: avatar},
-        process.env.ACCESS_TOKEN_SECRET,
-        {expiresIn: '23d'}
-    );
-};
--- backend/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createUser, getUser } = require('../controllers/userController');
-const router = express.Router();
-
-router.post('/create',createUser);
-router.get('/get/:email',getUser);
-
-module.exports = router;
