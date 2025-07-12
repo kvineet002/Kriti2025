4fc396b51e758369c48afa19db57057e253f281b
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2025-07-12 16:07:50 +0530

    Update ChatSection.js

--- frontend/src/components/Home/ChatSection.js
+++ frontend/src/components/Home/ChatSection.js
@@ -45,7 +45,7 @@ function ChatSection({ setHtmlCode, htmlCode, sandPackWidth }) {
   
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-pro",
+    model: "gemini-2.5-flash",
     systemInstruction: systemPrompt,
   });
 
